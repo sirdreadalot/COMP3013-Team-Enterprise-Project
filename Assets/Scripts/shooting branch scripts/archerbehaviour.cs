a0fc3e8d9c5036c59eafd40e022cfa7ee3701f1f
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2021-11-25 20:00:08 +0000

    comments and set up for future features

--- Assets/Scripts/shooting branch scripts/archerbehaviour.cs
+++ Assets/Scripts/shooting branch scripts/archerbehaviour.cs
@@ -5,12 +5,12 @@ using System.Linq;
 
 public class archerbehaviour : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> targetlist = new List<GameObject>();
+    [SerializeField] private List<GameObject> targetlist = new List<GameObject>(); //will store all enemies in give collision radius
     
-   [SerializeField]  GameObject Target;
+   [SerializeField]  GameObject Target;  //store gameobject for current target
 
-    [SerializeField] private float closestEnemy = 0;
-    [SerializeField] private float dist = 0;
+    [SerializeField] private float closestEnemy = 0;   //stores closest distance from gameobject to this
+    [SerializeField] private float newdDist = 0;  //stores individual gameobject distances
     
 
 
@@ -29,32 +29,51 @@ public class archerbehaviour : MonoBehaviour
     void Update()
     {
 
+        //if(closesttarget == on)
+        targetclosest();   
+
+        //if(targetfirst == on)
+        //Target = targetlist[0];
+
+        //if(targetLast == on)
+        //Target = targetlist.LastOrDefault();
+
+
+
+
+
+    }
+
+    void targetclosest()
+    {
+
+
         if (targetlist.Count() == 0)
         {
             Target = null;
-            closestEnemy = 0;
-            dist = 0;
+            closestEnemy = 0;               //resets values when there are no targets and stops it hitting the for loop
+            newdDist = 0;
         }
         else
         if (targetlist.Count() != 0)
         {
 
-            closestEnemy = Vector2.Distance(this.transform.position, targetlist[0].transform.position);
+            closestEnemy = Vector2.Distance(this.transform.position, targetlist[0].transform.position); //if there is no enemies and one enters, this becomes the closest enemey
 
 
             for (int i = 0; i < targetlist.Count(); i++)
             {
 
-                dist = Vector2.Distance(this.transform.position, targetlist[i].transform.position);
+                newdDist = Vector2.Distance(this.transform.position, targetlist[i].transform.position);    //for each frame the targetlist is evaluated and stores the Ith gameobjects distance from the tower
 
 
 
 
-                if (dist <= closestEnemy)
+                if (newdDist <= closestEnemy)       //compares the previous distance (closestEnemy) to the new one found and stored in newdist. will pass if the newdist is smaller than the previous distance caught
                 {
 
-                    Target = targetlist[i];
-                    dist = closestEnemy;
+                    Target = targetlist[i];     //sets the new target to the gameobject found to be closer
+                    newdDist = closestEnemy;     //sets the new benchmark to beat for the next game object
 
                 }
 
@@ -64,12 +83,12 @@ public class archerbehaviour : MonoBehaviour
 
         }
 
-  
+
+
 
     }
 
 
-    
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -78,7 +97,7 @@ public class archerbehaviour : MonoBehaviour
 
       
       
-        targetlist.Add(collision.gameObject);
+        targetlist.Add(collision.gameObject);       //when a Gameobject gets in range its added to a list of potential targets
   
 
 
@@ -87,7 +106,7 @@ public class archerbehaviour : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
 
-        targetlist.Remove(collision.gameObject);
+        targetlist.Remove(collision.gameObject);        //once it leave the radius its taken off the targets list
 
       
 
